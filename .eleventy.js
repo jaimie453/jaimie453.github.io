349b3486dd5ada6b9436573f14ace787a2affcdd
Jaimie Bowen <jaimie6453@gmail.com>
2022-10-29 16:25:22 -0400

    added footer and theme switcher

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,6 @@
-const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/style.css");
     eleventyConfig.addWatchTarget("./src/sass/");
-    eleventyConfig.addPlugin(syntaxHighlight);
     
     return {
       dir: {
