34773a91209e6bdaf31801f2e629ab0fe78301fb
Jaimie Bowen <jaimie6453@gmail.com>
2022-10-31 01:48:30 -0400

    Updates

---
+++ assets/js/index.js
@@ -0,0 +1,82 @@
+//
+// show the correct favicon in light/dark mode
+//
+const faviconPngLink = document.getElementById("faviconPng");
+const usingDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+if (!usingDarkMode) faviconPngLink.href = `/assets/images/favicon-light.png`
+
+window.matchMedia('(prefers-color-scheme: dark)').onchange = (event) => {
+    const theme = event.matches ? "dark" : "light";
+    faviconPngLink.href = `/assets/images/favicon-${theme}.png`
+};
+
+
+//
+// handle mobile nav menu interaction
+//
+const header = document.getElementById("header");
+const htmlTag = document.getElementById("html");
+const menuTrigger = document.getElementById("navMenuTrigger");
+let resizeTimeout;
+
+function toggleMenu() {
+    header.classList.toggle("header__open");
+    htmlTag.classList.toggle("hide-scroll");
+
+    const isMenuOpen = header.classList.contains("header__open");
+
+    if (isMenuOpen) {
+        menuTrigger.innerHTML = "Close";
+        document.addEventListener("keydown", handleMenuEscape);
+        header.addEventListener("focusout", handleFocusOutside);
+        window.addEventListener("resize", handleResize);
+    } else {
+        menuTrigger.innerHTML = "Menu";
+        document.removeEventListener("keydown", handleMenuEscape);
+        header.removeEventListener("focusout", handleFocusOutside);
+        window.removeEventListener("resize", handleResize);
+    }
+}
+
+function handleMenuEscape(event) {
+    if (event.key === 'Escape') {
+        closeMenu();
+        menuTrigger.focus();
+    }
+}
+
+function handleResize() {
+    if (!!resizeTimeout) {
+        clearTimeout(resizeTimeout);
+    }
+
+    resizeTimeout = setTimeout(function () {
+        console.log("closing");
+        const fontSizeInPx = window.getComputedStyle(htmlTag).fontSize;
+        const fontSizeInRem = parseFloat(fontSizeInPx.substr(0, fontSizeInPx.indexOf("px")));
+        const windowWidthInRem = window.innerWidth / fontSizeInRem;
+        const navMenuMaxWidth = 36; // rem value in sass
+
+        if (windowWidthInRem > navMenuMaxWidth) {
+            closeMenu();
+        }
+    }, 250);
+}
+
+function handleFocusOutside(event) {
+    const isFocusInsideHeader = header.contains(event.relatedTarget)
+    if (!isFocusInsideHeader) {
+        closeMenu();
+    }
+}
+
+function closeMenu() {
+    header.classList.remove("header__open");
+    htmlTag.classList.remove("hide-scroll");
+
+    menuTrigger.innerHTML = "Menu";
+    document.removeEventListener("keydown", handleMenuEscape);
+    document.removeEventListener("resize", handleResize);
+}
+
