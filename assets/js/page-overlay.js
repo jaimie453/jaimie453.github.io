d60f50711121a8102569ffe91c4bb217b7219506
Jaimie <jaimie6453@gmail.com>
2022-12-28 22:55:45 -0500

    Updates

---
+++ assets/js/page-overlay.js
@@ -0,0 +1,57 @@
+//
+// handle page fade
+//
+// TODO: check for reduced motion preference before doing this
+
+document.addEventListener("DOMContentLoaded", () => {
+  handleLinkClicks();
+});
+
+function handleLinkClicks() {
+  document.addEventListener("click", (event) => {
+    let clickTarget = event.target;
+
+    // get parent <a> if a child element was clicked
+    while (clickTarget && clickTarget.nodeName !== "A") {
+      clickTarget = clickTarget.parentNode;
+    }
+
+    const prefersReducedMotion = window.matchMedia(
+      "(prefers-reduced-motion: reduce)"
+    ).matches;
+
+    if (!clickTarget) {
+      return;
+    } else if (isInternalLink(clickTarget) && !prefersReducedMotion) {
+      fadeOutPage(event, clickTarget.href);
+    }
+  });
+}
+
+// ignore id links on the current page e.g. localhost/#main
+function isInternalLink(link) {
+  const inSameDomain = link.origin === window.location.origin;
+  const linksToIdOnCurrentPage =
+    link.pathname === window.location.pathname && link.href.includes("#");
+  const isResumeLink = link.href.includes("resume.pdf");
+
+  return inSameDomain && !linksToIdOnCurrentPage && !isResumeLink;
+}
+
+function fadeOutPage(linkEvent, href) {
+  const isMenuOpen = navContent.classList.contains("nav__content--open");
+
+  let fadeOutTarget = isMenuOpen ? navContent : pageOverlay;
+  fadeOutTarget.addEventListener("animationend", () => {
+    window.location = href;
+  });
+
+  linkEvent.preventDefault();
+
+  if (isMenuOpen) {
+    navContent.classList.remove("fade-in");
+    navContent.classList.add("fade-out");
+  } else {
+    pageOverlay.classList.add("fade-in");
+  }
+}
